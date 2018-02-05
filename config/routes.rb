f02687ce7b0026ab48dd4059ccb9ee7a503f4950
Vadim T <the.tarasov@gmail.com>
2018-02-06 01:54:46 +0200

    Small fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 WordsCounter::Application.routes.draw do
-  resources :widgets
   post 'counter' => 'counter#create'
   post 'counter_from_file' => 'counter#create_from_file'
   get 'statistics' => 'statistics#show'
